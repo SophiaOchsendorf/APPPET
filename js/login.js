f8c0f7176bb76e0cf0a58cf17f42f8996c845cec
SophiaOchsendorf <105024506+SophiaOchsendorf@users.noreply.github.com>
2023-10-04 20:31:09 -0300

    Add files via upload

---
+++ js/login.js
@@ -0,0 +1,115 @@
+function login() {
+    //console.log("aqui");
+    var dados = $("form-login").serialize();
+    console.log(dados);
+
+    var usuario = $ ("usuario").val();
+    //console.log(usuario);
+
+    var senha = $ ("#senha").val();
+    //console.log(senha);
+
+    $.ajax({
+        type: 'POST',//método
+        url: 'valida.php',
+        cache: false,
+        //dataType:'JSON',
+        //data: 'usuario='+usuario+'senha='+senha,
+        data: dados,
+
+
+        success:function (e) {
+            //var r=e;
+            //console.log(r);
+
+            console.log("entrei no ajax");
+            console.log(e);
+
+            if(e=='1'){
+
+                console.log('Sucesso 1');
+                setTextToNoAlert('alert btn-success text-center','Sucesso!');
+                setTimeout("window.location.href='administrativo.php'", 3000);
+            }
+            else if(e=='2'){
+                console.log('erro 2');
+                setTextToNoAlert('alert btn-danger text-center','Erro no Login! Senha');
+
+                //setTimeout("window.location.href='login.php'",5000);
+
+
+            }
+            else if(e== '4'){
+
+                console.log('erro 4');
+                setTextToNoAlert('alert btn-danger text-center','Erro no Login! Usuário');
+
+                //setTimeout("window.location.href='login.php'", 5000);
+
+
+            }
+            else{
+
+                console.log('erro 3');
+                setTextToNoAlert('alert btn-danger text-center','Erro no Login!');
+
+                //setTimeout("window.location.href='login.php'", 5000);
+
+
+            }
+        }
+    });
+}
+
+
+function setTextToNoAlert(classe, message) {
+    $('.alert').attr('class', classe);
+    $('#mensagem').text(message);
+
+}
+
+
+$(document).ready(function() {
+
+    // Add smooth scrolling to all links in navbar + footer link
+
+    $(".navbar a, footer a[href='#topo']").on('click', function(event)) {
+
+        //Make sure this.hash has a value before overriding defalt behavior
+
+        if (this.hash !== ""){
+
+            //Prevent defalt anchor click behavior
+
+            event.preventDefault();
+
+            //Store hash
+
+            var hash = this.hash;
+
+            //Using jQuery's animate() method to add smooth page scroll
+            //the optional number (900) specifies the number of milliseconds it takes to scroll to the specified area
+
+            $('html, body').animate({
+                scrollTop: $(hash).offfset().top
+            }, 900, function(){
+
+                //Add hash (#) to URL when done scrolling (defalt click behavior)
+
+                window.location.hash = hash;
+            });
+        } // End if
+    }
+
+
+  $(window).scroll(function(){
+    $(".slideanim").each(function(){
+        var pos = $(this).offset().top;
+
+        var winTop = $(window).scrollTop();
+        if (pos < winTop + 600)  {
+            $(this).addclass("slide");
+        }
+    });
+  });
+});
